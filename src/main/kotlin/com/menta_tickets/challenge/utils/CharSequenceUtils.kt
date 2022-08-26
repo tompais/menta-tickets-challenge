e4b800fe52499949335c46e9d824260b918e94d5
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2022-08-26 16:19:07 -0300

    improving code

--- src/main/kotlin/com/menta_tickets/challenge/utils/CharSequenceUtils.kt
+++ src/main/kotlin/com/menta_tickets/challenge/utils/CharSequenceUtils.kt
@@ -3,21 +3,20 @@ package com.menta_tickets.challenge.utils
 import com.menta_tickets.challenge.utils.RegexUtils.ONE_CLOSED_STROKE
 import com.menta_tickets.challenge.utils.RegexUtils.THREE_CLOSED_STROKES
 import com.menta_tickets.challenge.utils.RegexUtils.TWO_CLOSED_STROKES
-import com.menta_tickets.challenge.utils.RegexUtils.countAllAndMultiply
+import com.menta_tickets.challenge.utils.RegexUtils.countAll
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.async
+import kotlinx.coroutines.supervisorScope
+import kotlinx.coroutines.withContext
 
 object CharSequenceUtils {
-    suspend fun CharSequence.countClosedStrokes(): UInt = flow {
-        emitAll(ONE_CLOSED_STROKE.countAllAndMultiply(this@countClosedStrokes, 1u))
-        emitAll(
-            TWO_CLOSED_STROKES.countAllAndMultiply(this@countClosedStrokes, 2u)
-        )
-        emitAll(
-            THREE_CLOSED_STROKES.countAllAndMultiply(this@countClosedStrokes, 3u)
-        )
-    }.flowOn(Dispatchers.Default).toList().sum()
+    suspend fun CharSequence.countClosedStrokes(): UInt = supervisorScope {
+        withContext(Dispatchers.Default) {
+            val deferredValue1 = async { ONE_CLOSED_STROKE.countAll(this@countClosedStrokes) }
+            val deferredValue2 = async { TWO_CLOSED_STROKES.countAll(this@countClosedStrokes) * 2u }
+            val deferredValue3 = async { THREE_CLOSED_STROKES.countAll(this@countClosedStrokes) * 3u }
+
+            deferredValue1.await() + deferredValue2.await() + deferredValue3.await()
+        }
+    }
 }
--- src/main/kotlin/com/menta_tickets/challenge/utils/RegexUtils.kt
+++ src/main/kotlin/com/menta_tickets/challenge/utils/RegexUtils.kt
@@ -1,21 +1,22 @@
 package com.menta_tickets.challenge.utils
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
 
 object RegexUtils {
     val ONE_CLOSED_STROKE = Regex("[0469ADOPQRabdegijopq@#?¿À-ÃÒ-Õà-ãè-êò-õ]+")
     val TWO_CLOSED_STROKES = Regex("[8BÅËÏÜåïüÿ\$%∞&]+")
     val THREE_CLOSED_STROKES = Regex("[ÄÖäëö‰]+")
 
-    fun Regex.countAllAndMultiply(
-        input: CharSequence,
-        multiplier: UInt = 1u
-    ): Flow<UInt> = findAll(input).asFlow()
+    suspend fun Regex.countAll(
+        input: CharSequence
+    ): UInt = findAll(input).asFlow()
         .flowOn(Dispatchers.Default)
-        .map { it.value.count().toUInt() * multiplier }
+        .map { it.value.count().toUInt() }
         .flowOn(Dispatchers.Default)
+        .toList()
+        .sum()
 }
